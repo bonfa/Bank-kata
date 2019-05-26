0f36f8a30b4374a41c8de71f3db47a809ae4be4f
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2019-05-26 10:37:39 +0200

    further refactoring before starting a new test

--- app/__test__/AccountTest.ts
+++ app/__test__/AccountTest.ts
@@ -1,28 +1,35 @@
 import {Account} from "../source/Account";
-import {Mock, Times} from "typemoq";
+import {IMock, Mock, Times} from "typemoq";
 import {TransactionRepository} from "../source/TransactionRepository";
 import {Transaction} from "../source/Transaction";
 
 describe('Account', () => {
-  describe('when deposit some money', () => {
-    it('should store the transaction in the repository', () => {
-      let transactionRepository = Mock.ofType<TransactionRepository>();
-      let account = new Account(transactionRepository.object);
+  let transactionRepository: IMock<TransactionRepository>;
+  let account: Account;
 
+  beforeEach(() => {
+    transactionRepository = Mock.ofType<TransactionRepository>();
+    account = new Account(transactionRepository.object);
+  });
+
+  describe('when depositing some money', () => {
+    it('should store the transaction in the repository', () => {
       account.deposit(400);
 
       transactionRepository.verify(it => it.addTransaction(Transaction.depositOf(400)), Times.once());
     });
   });
 
-  describe('when withdraw some money', () => {
+  describe('when withdrawing some money', () => {
     it('should store the transaction in the repository', () => {
-      let transactionRepository = Mock.ofType<TransactionRepository>();
-      let account = new Account(transactionRepository.object);
-
       account.withdraw(300);
 
       transactionRepository.verify(it => it.addTransaction(Transaction.withdrawOf(300)), Times.once());
     });
   });
+
+  // describe('when printing the statement', () => {
+  //   it('it should call the statement printer with the proper transaction', () => {
+  //   });
+  // });
 });
