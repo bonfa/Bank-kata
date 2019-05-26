4b7dff039f2925bedfe9eb67ea6964978048abce
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2019-05-26 10:16:02 +0200

    now we added the acceptance test

---
+++ app/__test__/AcceptanceTest.ts
@@ -0,0 +1,16 @@
+import {Mock, Times} from "typemoq";
+import {Account} from "../source/Account";
+import {Output} from "../source/Output";
+
+describe('It works', () => {
+  const output = Mock.ofType<Output>();
+  const account = new Account(undefined, calendar.object);
+  account.deposit(500);
+  account.withdraw(100);
+
+  account.printBalance();
+
+  output.verify(it=>it.print("Date | Amount | Balance"), Times.exactly(1));
+  output.verify(it=>it.print("24.12.2015 | +500 | 500"), Times.exactly(1));
+  output.verify(it=>it.print("23.8.2016 | -100 | 400"), Times.exactly(1));
+});
